6cca0d7278b5ecb9741b0bf058f370a940b32111
Vlad Tsyrklevich <vlad@tsyrklevich.net>
2018-06-10 09:52:28 -0700

    Sort output for better diffs

--- parse.rb
+++ parse.rb
@@ -26,25 +26,16 @@ types.each { |t|
   end
 }
 
-puts <<-END
-enum __MIGDirection { IN, INOUT, OUT, SERVERAUDITTOKEN, REQUESTPORT, MSGOPTION };
-struct {
-  int idx;
-  enum __MIGDirection direction;
-  bool is_unlimited_size_array;
-  const char *routine;
-  const char *argument_name; 
-} mig_routines[] = {
-END
+output = {}
 
-processed_routines = Set.new
 routines.each { |r|
   # Routines can be included multiple times in the master MIG output.
-  next if processed_routines.include?(r[0].to_s)
+  name = r[0].to_s
+  next if output[name]
 
+  output[name] = []
   idx = 0
   r[1, r.length].each { |arg|
-#    puts "/* #{arg.inspect} */"
     arg_type = arg[1][0].to_s
     direction = 'IN'
     direction = arg[2][0].to_s.upcase if arg[2].to_s == 'DIRECTION'
@@ -54,11 +45,11 @@ routines.each { |r|
 
     is_array = array_type_map[arg_type] ? true : false
 
-    print "  { #{idx}, #{direction}, #{is_array}, \"#{r[0]}\", \"#{arg[0]}\" },"
-    print " // #{ipc_flags}" if ipc_flags
-    print " // #{array_type_map[arg_type]}" if is_array
-    puts
+    comment = []
+    comment << ipc_flags if ipc_flags
+    comment << array_type_map[arg_type] if is_array
 
+    output[name] << [idx, direction, is_array, arg[0], comment.join(', ')]
     idx += 1
 
     # FIXED_SIZE does not add a no count
@@ -66,12 +57,32 @@ routines.each { |r|
     if array_type_map[arg_type] && array_type_map[arg_type] != 'FIXED_SIZE' && (!ipc_flags || ipc_flags != 'SameCount')
       idx_direction = 'OUT'
       idx_direction = 'INOUT' if ipc_flags && ipc_flags == 'CountInOut'
-      puts "  { #{idx}, #{idx_direction}, false, \"#{r[0]}\", \"#{arg[0]}_size\" }, // Auto-generated"
-
+      output[name] << [idx, idx_direction, false, "#{arg[0]}_size", 'Auto-generated' ]
       idx += 1
     end
   }
-
-  processed_routines.add(r[0].to_s)
 }
+
+output = output.sort_by { |k, v| k.downcase }
+
+puts <<-END
+enum __MIGDirection { IN, INOUT, OUT, SERVERAUDITTOKEN, REQUESTPORT, MSGOPTION };
+struct {
+  int idx;
+  enum __MIGDirection direction;
+  bool is_unlimited_size_array;
+  const char *routine;
+  const char *argument_name;
+} mig_routines[] = {
+END
+
+output.each { |routine, args|
+  args.each { |idx, direction, is_array, arg_name, comment|
+
+    print "  { #{idx}, #{direction}, #{is_array}, \"#{routine}\", \"#{arg_name}\" },"
+    print " // #{comment}" if comment && comment != ''
+    puts
+  }
+}
+
 puts '};'
